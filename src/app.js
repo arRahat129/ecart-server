f90d5cd8674e4456171c8c51a6fb723ba200ba86
arRahat129 <rahashik129@gmail.com>
2026-09-06 15:49:28 +0600

    placed dotenv config in the app.js file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require('dotenv');
+dotenv.config();
 const authRoutes = require("./routes/auth.routes");
 const customerRoutes = require("./routes/customer.routes");
 const productRoutes = require("./routes/product.routes");
